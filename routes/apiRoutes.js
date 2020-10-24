8eb1ffcbf64a5333fdbcdbc80e65b6c254263389
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-24 11:12:10 -0400

    Done.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,7 @@ const DbFunction = require("../db/dbFunctions");
 
 const dbFunc = new DbFunction();
 
+//GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 router.get("/notes", async (req, res) => {
   try {
     const notes = await dbFunc.getNotes();
@@ -12,6 +13,7 @@ router.get("/notes", async (req, res) => {
   }
 });
 
+//POST `/api/notes` - Should receive a new note to save, add to db.json file, and return the new note to the client.
 router.post("/notes", async (req, res) => {
   try {
     const newNote = req.body;
@@ -22,6 +24,8 @@ router.post("/notes", async (req, res) => {
   }
 });
 
+
+//DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete and delete a note from all notes.
 router.delete("/notes/:id", (req, res) => {
   res.json(dbFunc.deleteNote(req.params.id));
 });
